584ed10557125ae3b0fc62541c9983962831c7ac
Lucas Heymès <lucas@lovethebomb.eu>
2020-05-29 15:54:36 +0200

    Update Time Travel feature with Vercel URLs

--- static/js/timetravel.js
+++ static/js/timetravel.js
@@ -1,5 +1,12 @@
-const contentPref = "https://deploy-preview-";
-const contentPost = "--design-oxide-computer.netlify.com/";
+const previewURLPrefix = "https://design-site-git";
+const previewURLSuffix = ".oxidecomputer.now.sh";
+
+const $timeFrame = document.querySelector("#time-frame");
+const $prTitle = document.querySelector("#prTitle");
+const $prLink = document.querySelector("#prLink");
+const $prAvatar = document.querySelector("#prAvatar");
+const $prIndex = document.querySelector("#prIndex");
+const $prCount = document.querySelector("#prCount");
 
 let prIndex = 0;
 
@@ -28,11 +35,23 @@ function lastPR() {
 }
 
 function changeContent() {
-  pr = prMap.get(prNumbers[prIndex]);
-  document.getElementById("myframe").src = contentPref + pr["number"] + contentPost;
-  document.getElementById("prTitle").textContent = pr["title"];
-  document.getElementById("prLink").href = pr["html_url"];
-  document.getElementById("prAvatar").src = pr["user"]["avatar_url"];
-  document.getElementById("prIndex").textContent = prIndex + 1;
-  document.getElementById("prCount").textContent = prNumbers.length;
+  const pr = prMap.get(prNumbers[prIndex]);
+  // TODO: Replace checks with ES2020 Optional Chaining
+  // to prevent breaking when repo does not exist anymore
+  const isFork = pr.head.repo && pr.head.repo.fork || false;
+  const owner = pr.head.repo && pr.head.repo.owner.login || '';
+  const ref = pr.head.ref;
+
+  // Will be: prefix-fork-owner-ref-suffix
+  // Or: prefix-ref-suffix
+  $timeFrame.src = `${previewURLPrefix}${isFork ? `-fork-${owner}` : ''}-${ref}${previewURLSuffix}`;
+  $prTitle.textContent = pr.title;
+  $prLink.href = pr.html_url;
+  $prAvatar.src = pr.user.avatar_url;
+  $prIndex.textContent = prIndex + 1;
+  $prCount.textContent = prNumbers.length;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  lastPR();
+});
