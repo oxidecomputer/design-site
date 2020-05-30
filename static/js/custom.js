a362e55d9a8bbdf37105e29448fe6063bf133fa8
Lucas Heymès <lucas@lovethebomb.eu>
2020-05-30 13:49:25 +0200

    Set theme dectection before body rendering

--- static/js/custom.js
+++ static/js/custom.js
@@ -9,15 +9,6 @@ function switchTheme(e) {
   localStorage.setItem("theme", theme);
 }
 
-function selectedTheme() {
-  return localStorage.getItem("theme");
-}
-
-const currentTheme = selectedTheme();
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-}
-
 const mql = matchMedia("(prefers-color-scheme: dark)");
 if ((mql.matches && currentTheme != "light") || currentTheme === "dark") {
   themeToggle.checked = true;
